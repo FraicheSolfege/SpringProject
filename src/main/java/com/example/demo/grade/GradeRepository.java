84c28f8ac8dc1074a3204f59348ffdbe1c02a523
FraicheSolfege <littlenathan68@yahoo.com>
2024-07-22 22:49:19 -0500

    hurry up!!

--- src/main/java/com/example/demo/grade/GradeRepository.java
+++ src/main/java/com/example/demo/grade/GradeRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.grade;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GradeRepository extends JpaRepository<Grade, Long> {
 
+    @Query("SELECT g FROM Grade g WHERE g.student.id = ?1")
+    Grade findGradeByStudentId(Long studentId);
 }
